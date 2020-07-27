13bc0a507ad63e11d364b347f8564044d3fee874
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2020-07-27 20:27:06 -0300

    useStyles

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export * from './src/ColorScheme'
 export * from './src/Theme'
+export * from './src/Styles'
---
+++ src/Styles.ts
@@ -0,0 +1,12 @@
+import React, { useMemo } from 'react'
+import { Theme, useTheme } from './Theme'
+import { StyleSheet } from 'react-native'
+
+export function useStyles<T extends Theme, S extends StyleSheet.NamedStyles<S> | StyleSheet.NamedStyles<any>>(styleFactory: (theme: T) => S | StyleSheet.NamedStyles<S>) : { styles: S, theme: T } {
+
+    const theme = useTheme() as T
+    
+    const styles = useMemo(() => StyleSheet.create(styleFactory(theme)), [theme, styleFactory])
+
+    return { styles, theme }
+}
